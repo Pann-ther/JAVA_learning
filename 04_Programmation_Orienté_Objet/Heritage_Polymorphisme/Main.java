5f44cf8f4fbe85fdaef04cc06f3ff245c9246049
Pann-ther <projet.chebbah@gmail.com>
2024-10-18 12:56:47 +0200

    optimisation de l'utilistion du polymorphisme avec le tableau

--- 04_Programmation_Orienté_Objet/Heritage_Polymorphisme/Main.java
+++ 04_Programmation_Orienté_Objet/Heritage_Polymorphisme/Main.java
@@ -11,31 +11,21 @@ public class Main {
         Moto cb650r = new Moto("cb650r", 200);
         Moto panigale = new Moto("panigale",250);
 
-        // Creation d'un tableau de type voiture
-        Voiture[] parkingVoiture = new Voiture[3];
+        // Creation d'un tableau de type vehicule
+        Vehicule[] parking = new Vehicule[6];
 
-        // Initialisation du tableau avec les objets de la classe voiture
-        parkingVoiture[0] = clio;
-        parkingVoiture[1] = audi;
-        parkingVoiture[2] = porsche;
-
-        // Creation d'un tableau de type moto
-        Moto[] parkingMoto = new Moto[3];
-
-        // Initialisation du tableau avec les objets de la classe moto 
-        parkingMoto[0] = mt07;
-        parkingMoto[1]= cb650r;
-        parkingMoto[2] = panigale;
-
-
-        // boucle for-each pour afficher la vitesse de chaque vehicule dans les tableaux
-        for(Moto moto : parkingMoto){
-            moto.afficherVitesse();
+        parking[0]= clio;
+        parking[1]= audi;
+        parking[2]= porsche;
+        parking[3]= mt07;
+        parking[4]= cb650r;
+        parking[5]= panigale;
+        
+        for(Vehicule vehicule : parking){
+            vehicule.afficherVitesse();
         }
 
-        for(Voiture voiture : parkingVoiture){
-            voiture.afficherVitesse();
-        }
+        
 
     }
 }
