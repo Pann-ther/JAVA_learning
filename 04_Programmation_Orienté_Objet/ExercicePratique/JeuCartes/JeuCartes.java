b79b662b009c9bfb7d1ce9705a4838d0cf2e1f8b
Pann-ther <projet.chebbah@gmail.com>
2024-11-13 13:57:13 +0100

    suppression du doublon de la methode main

--- 04_Programmation_Orienté_Objet/ExercicePratique/JeuCartes/JeuCartes.java
+++ 04_Programmation_Orienté_Objet/ExercicePratique/JeuCartes/JeuCartes.java
@@ -49,15 +49,4 @@ public class JeuCartes{
 	public String toString(){
 		return "La carte est un "+getNomValeur()+" de "+couleur;
 	}
-
-    // Methode main
-    public static void main(String[] args) {
-        JeuCartes carte1 = new JeuCartes("Coeur",13);
-        JeuCartes carte2 = new JeuCartes("Pique",4);
-        System.out.println(carte1.getCouleur());
-        System.out.println(carte2.getValeur());
-        System.out.println(carte2.plusForte(carte1).toString());
-        System.out.println(carte1.toString());
-        System.out.println(carte2.toString());
-    }
 }
