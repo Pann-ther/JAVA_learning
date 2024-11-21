f6b21f099976e0c5f9776f2c6c8f078b779f2956
Pann-ther <projet.chebbah@gmail.com>
2024-11-22 00:58:33 +0100

    ajout tab et boucle pour le troupeau

--- 04_Programmation_Orienté_Objet/ExercicePratique/Vache/VacheMain.java
+++ 04_Programmation_Orienté_Objet/ExercicePratique/Vache/VacheMain.java
@@ -1,10 +1,14 @@
 public class VacheMain {
     public static void main(String[] args) {
         Vache.setPrixKilo(3);
-        Vache vache1 = new Vache(670);
-        Vache vache2 = new Vache(890);
-        Vache vache3 = new Vache(1100);
-        double prixTroupeau = vache1.getPrix()+vache2.getPrix()+vache3.getPrix();
+        Vache [] troupeau = new Vache[3];
+        troupeau[0] = new Vache(670);
+        troupeau[1] = new Vache(890);
+        troupeau[2] = new Vache(1100);
+        double prixTroupeau = 0;
+        for(int i = 0; i<troupeau.length; i++){
+            prixTroupeau += troupeau[i].getPrix();
+        }
         System.out.println("Le prix du troupeau est de "+String.format("%.2f",(prixTroupeau))+"€");
     }
 }
