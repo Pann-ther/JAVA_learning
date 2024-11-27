b65c6d68139ff1ba55b0370a2453df29381dcc23
Pann-ther <projet.chebbah@gmail.com>
2024-11-27 19:44:23 +0100

    brouillon

--- 04_Programmation_Orienté_Objet/Brouillon/TabDouble/tableaudouble.java
+++ 04_Programmation_Orienté_Objet/Brouillon/TabDouble/tableaudouble.java
@@ -19,5 +19,27 @@ public class tableaudouble {
         for(int i =0; i<delTab.length; i++){
             System.out.println("Variation du tonnage en " + (1939 + i) + ": " +delTab[i]);
         }
+
+        // Notes des eleves
+        double[] notesEleve1= {10, 13.5, 15.5, 5};
+        double[] notesEleve2= {17, 13.5, 5, 5};
+        double[] notesEleve3= {13, 5, 10.5, 9};
+        double[] notesEleve4= {6, 9.5, 18.5, 12};
+        double[][] tabNotes = new double[4][];
+        // Affectation des notes des eleves au tableau de notes du groupe
+        tabNotes[0] = notesEleve1;
+        tabNotes[1] = notesEleve2;
+        tabNotes[2] = notesEleve3;
+        tabNotes[3] = notesEleve4;
+        // Calcul la motyenne de chaque eleve
+        for(int l=0; l<tabNotes.length; l++){
+            double somme = 0;
+            for(int c=0; c<tabNotes[l].length; c++){
+                somme += tabNotes[l][c];
+            }
+            double moyenne = somme / tabNotes[l].length;
+            System.out.println("Moyenne de l'eleve "+(l+1)+": "+moyenne);
+        }
     }
+
 }
