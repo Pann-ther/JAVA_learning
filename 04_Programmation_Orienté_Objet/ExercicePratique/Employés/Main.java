2cae3e8285f6f5fbbe4b3bf1ae7b5b3e72e70b79
Pann-ther <projet.chebbah@gmail.com>
2024-12-02 21:33:40 +0100

    saving

--- 04_Programmation_Orienté_Objet/ExercicePratique/Employés/Main.java
+++ 04_Programmation_Orienté_Objet/ExercicePratique/Employés/Main.java
@@ -1,19 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        try{
-        Employés employé1 = new Employés("John Doe", "12345", 2);
-        Employés employé2 = new Employés("Janne Su", "234", 2);
-        Commercial commercial1 = new Commercial("Barry Stone", "200", 2, 40000);
-        Commercial commercial2 = new Commercial("Pitt Altor", "400", 2, 25000);
-        Personnel liste = new Personnel();
-        liste.ajouterEmploye(employé1);
-        liste.ajouterEmploye(employé2);
-        liste.ajouterEmploye(commercial1);
-        liste.ajouterEmploye(commercial2);
-        System.out.println(liste.salairePersonnel());
-        } catch (Exception e){
+        try {
+            Employés employé1 = new Employés("John Doe", "12345", 2);
+            Employés employé2 = new Employés("Janne Su", "234", 2);
+            Commercial commercial1 = new Commercial("Barry Stone", "200", 2, 40000);
+            Commercial commercial2 = new Commercial("Pitt Altor", "400", 2, 25000);
+            Personnel liste = new Personnel();
+            liste.ajouterEmploye(employé1);
+            liste.ajouterEmploye(employé2);
+            liste.ajouterEmploye(commercial1);
+            liste.ajouterEmploye(commercial2);
+            System.out.println(liste.salairePersonnel());
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-
     }
 }
